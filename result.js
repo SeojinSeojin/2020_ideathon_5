88dedd4a55cae70a975b06b903de0d92c52eed4d
mery0816 <mery0816@naver.com>
2020-06-08 18:47:10 +0900

    result

---
+++ result.js
@@ -0,0 +1,9 @@
+var i=Math.floor(Math.random()*data.result.length());
+
+if (result = rs){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+}
