4d2dc18459a992d8e3733de37be40a1ade50b846
Gahee Kim <gaheekim@Gahees-MacBook-Pro.local>
2020-06-09 15:40:04 +0900

    EDIT result.js

--- result.js
+++ result.js
@@ -6,4 +6,58 @@ if (result = rs){
     document.write(data[0].rs[i].description)
     document.write(data[0].rs[i].item);
     document.write(data[0].rs[i].service);
+} else if (result = rr){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+} else if (result = rp){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+} else if (result = rc){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+} else if (result = ra){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+} else if (result = is){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+} else if (result = ir){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+} else if (result = ip){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+} else if (result = ic){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
+} else if (result = ia){
+    document.write(data[0].korean);
+    document.write(data[0].rs[i].name);
+    document.write(data[0].rs[i].description)
+    document.write(data[0].rs[i].item);
+    document.write(data[0].rs[i].service);
 }
